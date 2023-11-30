980f3624513934344503ab14eaccf3d2c071ed4c
STAENLYZ <staenlymarcell@gmail.com>
2023-11-30 20:38:59 +0700

    Query Tabel SQL Server

--- Database/QUERY.sql
+++ Database/QUERY.sql
@@ -1,14 +1,36 @@
-CREATE TABLE Pelanggan(
-NamaPelanggan varchar(100) not null,
-NoRekening varchar(16) primary key not null,
-Alamat varchar(150) ,
-NoTelp varchar(14),
-Email varchar(255)
-)
+-- Tabel Keamanan
+CREATE TABLE Keamanan (
+    IdKeamanan INT PRIMARY KEY,
+    NoRekening NVARCHAR(50) UNIQUE,
+    LastAccesed NVARCHAR(50),
+    AccessHistory NVARCHAR(255)
+);
 
-CREATE TABLE Transaksi(
-NoRekening varchar(16) foreign key references Pelanggan(NoRekening),
-JenisTransaksi varchar(50) not null,
-TanggalTransaksi datetime,
-JumlahTransaksi money
-)
+-- Tabel Pelanggan
+CREATE TABLE Pelanggan (
+    NoRekening NVARCHAR(50) PRIMARY KEY,
+    NamaPelanggan NVARCHAR(100),
+    AlamatPelanggan NVARCHAR(255),
+    NoTelp NVARCHAR(20),
+    EmailPelanggan NVARCHAR(100)
+);
+
+-- Tabel Tabungan
+CREATE TABLE Tabungan (
+    IdTabungan INT PRIMARY KEY,
+    NoRekening NVARCHAR(50) UNIQUE,
+    JenisTabungan NVARCHAR(50),
+    SaldoTabungan DECIMAL(18, 2),
+    TanggalBuka NVARCHAR(50),
+    FOREIGN KEY (NoRekening) REFERENCES Pelanggan(NoRekening)
+);
+
+-- Tabel Transaksi
+CREATE TABLE Transaksi (
+    IdTransaksi INT PRIMARY KEY,
+    NoRekening NVARCHAR(50),
+    JenisTransaksi NVARCHAR(50),
+    TanggalTransaksi NVARCHAR(50),
+    DeskripsiTransaksi NVARCHAR(255),
+    FOREIGN KEY (NoRekening) REFERENCES Pelanggan(NoRekening)
+);
